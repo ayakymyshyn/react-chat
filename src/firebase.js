5f79101fede195b19ba55d33a3cd82b01ba47285
Andrii <andrewyakymyshyn@gmail.com>
2019-06-25 17:04:43 +0300

    edited gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/database";
 import "firebase/storage";
 
 var firebaseConfig = {
-    apiKey: "your key",
-    authDomain: "domain",
-    databaseURL: //ur url,
-    projectId: // your id,
-    storageBucket: //your bucket,
-    messagingSenderId: //your data,
-    appId: //your id
+    apiKey: "AIzaSyA9TpJMMVqUq81HFLExdhAogyeXkyxQkg0",
+    authDomain: "react-chat-feb7e.firebaseapp.com",
+    databaseURL: "https://react-chat-feb7e.firebaseio.com",
+    projectId: "react-chat-feb7e",
+    storageBucket: "react-chat-feb7e.appspot.com",
+    messagingSenderId: "75466828240",
+    appId: "1:75466828240:web:12ea9d786eb1f455"
   };
   firebase.initializeApp(firebaseConfig);
 
